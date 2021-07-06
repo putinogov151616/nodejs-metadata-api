36a409b84bb4cd5bd30f36bc9a5298a30c28ad60
putinogov151616 <86958345+putinogov151616@users.noreply.github.com>
2021-07-07 01:18:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const moment = require('moment')
 const { HOST } = require('./src/constants')
-const db = require('./src/database')
 
 const PORT = process.env.PORT || 5000
 
@@ -20,18 +19,10 @@ app.get('/', function(req, res) {
 
 app.get('/api/token/:token_id', function(req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
   const data = {
-    'name': person.name,
+    'name': parseInt(req.params.token_id).toString()
     'attributes': {
-      'birthday': person.birthday,
       'xxx': monthName(month),
-      'meme': memex(meme),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
     },
     'image': `${HOST}/images/${tokenId}.png`
   }
@@ -42,21 +33,8 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
 
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
-
 function monthName(month) {
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
   ]
   return monthNames[month - 1]
-}
-
-function memex(meme) {
-  const memexs = ["Jyyyy", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return memexs[meme - 1]
 }
