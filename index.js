5ebeeaf1377acfe47c340f3369fc802906c738b9
putinogov151616 <86958345+putinogov151616@users.noreply.github.com>
2021-07-07 01:33:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/api/token/:token_id', function(req, res) {
   const bdayParts = person.birthday.split(' ')
   const day = parseInt(bdayParts[1])
   const month = parseInt(bdayParts[0])
+  const meme = parseInt(bdayParts[0])
   const data = {
     'name': person.name,
     'attributes': {
