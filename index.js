4f5b352f45774ac2fcba40697c2447fdebc114c0
putinogov151616 <86958345+putinogov151616@users.noreply.github.com>
2021-07-25 21:08:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get('/api/token/:token_id', function(req, res) {
     'attributes': {
       'bccc': monthName(month),
       'meme': memeName(meme),
+      'meme1': meme1Name(meme1),
     },
     'image': `${HOST}/images/${tokenId}.png`
   }
@@ -48,4 +49,10 @@ function memeName(meme) {
   const memeNames = ["Flippin' Kindness", "They Were Good Kindness", "Bling-a-ding Alex", "Two, Three, Four Kindness", "Miss Piggy, If You're Ready? ", "A Smart State of Mind", "Alex Better have My Kindness", "Alex-Face Robbers", "I'm still Damian From the Streets", "Alex Said Go", "So Smart (I think I'll Scream)", "Follow the Kindness", "The Story of Miss Piggy", "Ultra-Damian Goes Flip", "I Ain't No Ronnie Kray", "The Miss Piggy Effect", "Don't Talk to Me About Kindness, yaz? ", "Damian is as Damian does", "I is Smart (And Don't You Forget It)", "Ultra-Edward Goes Flip", "Bling-a-ding Charlie", "I Ain't No Harold Shipman", "A Special State of Mind", "Two, Three, Four Love", "Don't Talk to Me About Love, yaz? ", "Edward is as Edward does", "The Story of Big Bird", "Charlie-Face Robbers", "They Were Good Love", "Charlie Said Go", "Charlie Better have My Love", "Follow the Love", "I'm still Edward From the Streets", "Flippin' Love", "The Big Bird Effect", "I is Special (And Don't You Forget It)", "Big Bird, If You're Ready? ", "So Special (I think I'll Scream)", "Ultra-Carla Goes Flip", "Carla is as Carla does", "I Ain't No Keyser Soze", "Don't Talk to Me About Kitten, yaz? ", "Follow the Kitten", "I'm still Carla From the Streets", "They Were Good Kitten", "I is Spiteful (And Don't You Forget It)", "Tigger Said Go", "Marge Simpson, If You're Ready? ", "Two, Three, Four Kitten", "The Story of Marge Simpson", "A Spiteful State of Mind", "So Spiteful (I think I'll Scream)", "Tigger-Face Robbers", "Bling-a-ding Tigger", "Tigger Better have My Kitten", "Flippin' Kitten", "The Marge Simpson Effect"
   ]
   return memeNames[meme - 1]
+}
+
+function meme1Name(meme1) {
+  const meme1Names = ["Flippin' Kindness", "They Were Good Kindness", "Bling-a-ding Alex", "Two, Three, Four Kindness", "Miss Piggy, If You're Ready? ", "A Smart State of Mind", "Alex Better have My Kindness", "Alex-Face Robbers", "I'm still Damian From the Streets", "Alex Said Go", "So Smart (I think I'll Scream)", "Follow the Kindness", "The Story of Miss Piggy", "Ultra-Damian Goes Flip", "I Ain't No Ronnie Kray", "The Miss Piggy Effect", "Don't Talk to Me About Kindness, yaz? ", "Damian is as Damian does", "I is Smart (And Don't You Forget It)", "Ultra-Edward Goes Flip", "Bling-a-ding Charlie", "I Ain't No Harold Shipman", "A Special State of Mind", "Two, Three, Four Love", "Don't Talk to Me About Love, yaz? ", "Edward is as Edward does", "The Story of Big Bird", "Charlie-Face Robbers", "They Were Good Love", "Charlie Said Go", "Charlie Better have My Love", "Follow the Love", "I'm still Edward From the Streets", "Flippin' Love", "The Big Bird Effect", "I is Special (And Don't You Forget It)", "Big Bird, If You're Ready? ", "So Special (I think I'll Scream)", "Ultra-Carla Goes Flip", "Carla is as Carla does", "I Ain't No Keyser Soze", "Don't Talk to Me About Kitten, yaz? ", "Follow the Kitten", "I'm still Carla From the Streets", "They Were Good Kitten", "I is Spiteful (And Don't You Forget It)", "Tigger Said Go", "Marge Simpson, If You're Ready? ", "Two, Three, Four Kitten", "The Story of Marge Simpson", "A Spiteful State of Mind", "So Spiteful (I think I'll Scream)", "Tigger-Face Robbers", "Bling-a-ding Tigger", "Tigger Better have My Kitten", "Flippin' Kitten", "The Marge Simpson Effect"
+  ]
+  return meme1Names[meme1 - 1]
 }
