b15ec13bf22124d09464126f860fe793a1e9bc9b
putinogov151616 <86958345+putinogov151616@users.noreply.github.com>
2021-07-29 09:16:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/api/token/:token_id', function(req, res) {
       'sauce cup': saucecupName(saucecup),
       'toothpick': toothpickName(toothpick),
     },
-    'image': `${HOST}/images/${tokenId}.png`
+    'image': `https://bafybeid6fk7gdxy4yovenpqc55mhc7ajsrxi4da5jckl2ykpe2bcagbkzq.ipfs.dweb.link/${tokenId}.jpg`
   }
   res.send(data)
 })
